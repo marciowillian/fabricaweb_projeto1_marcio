7f1bb25b24e55fa4430dd38987d1956d9520223f
marcio <willian_mw.cc@hotmail.com>
2016-11-24 00:44:07 -0300

    Atualizando meus arquivos

--- src/main/java/br/com/marciowillian/persistencia/jdbc/UsuarioDAO.java
+++ src/main/java/br/com/marciowillian/persistencia/jdbc/UsuarioDAO.java
@@ -2,73 +2,180 @@ package br.com.marciowillian.persistencia.jdbc;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import br.com.marciowillian.persistencia.entidade.Usuario;
 
 public class UsuarioDAO {
 	private Connection con = ConexaoFactory.getConnection();
-	
+
 	public void cadastrar(Usuario usu) {
 		String sql = "insert into usuario (nome, login, senha) values (?,?,?)";
-		
+
 		try {
-			//Criando um Stattement
+			// Criando um Stattement
 			PreparedStatement preparador = con.prepareStatement(sql);
-			preparador.setString(1, usu.getNome()); //substituindo o ?  pelo dado do usuario
+			preparador.setString(1, usu.getNome()); // substituindo o ? pelo
+													// dado do usuario
 			preparador.setString(2, usu.getLogin());
 			preparador.setString(3, usu.getSenha());
-			//Execultando o comando SQL no banco
+			// Execultando o comando SQL no banco
 			preparador.execute();
-			//Encerando o objeto preparador
+			// Encerando o objeto preparador
 			preparador.close();
-			
+
 		} catch (SQLException e) {
-			
+
 			e.printStackTrace();
 		}
-		
+
 	}
 
 	public void alterar(Usuario usu) {
-String sql = "update usuario set nome = ?, login = ?, senha = ? where id = ?";
-		
+		String sql = "update usuario set nome = ?, login = ?, senha = ? where id = ?";
+
 		try {
-			//Criando um Stattement
+			// Criando um Stattement
 			PreparedStatement preparador = con.prepareStatement(sql);
-			preparador.setString(1, usu.getNome()); //substituindo o ?  pelo dado do usuario
+			preparador.setString(1, usu.getNome()); // substituindo o ? pelo
+													// dado do usuario
 			preparador.setString(2, usu.getLogin());
 			preparador.setString(3, usu.getSenha());
 			preparador.setInt(4, usu.getId());
-			//Execultando o comando SQL no banco
+			// Execultando o comando SQL no banco
 			preparador.execute();
-			//Encerando o objeto preparador
+			// Encerando o objeto preparador
 			preparador.close();
+
+		} catch (SQLException e) {
+
+			e.printStackTrace();
+		}
+
+	}
+
+	public void excluir(Usuario usu) {
+		String sql = "delete from usuario where id = ?";
+
+		try {
+			// Criando um Stattement
+			PreparedStatement preparador = con.prepareStatement(sql);
+			preparador.setInt(1, usu.getId());
+			// Execultando o comando SQL no banco
+			preparador.execute();
+			// Encerando o objeto preparador
+			preparador.close();
+
+		} catch (SQLException e) {
+
+			e.printStackTrace();
+		}
+
+	}
+
+	public void salvar(Usuario usuario) {
+		if (usuario.getId() != null) {
+			alterar(usuario);
+		} else
+			cadastrar(usuario);
+	}
+
+	/**
+	 * Busca de um registro no banco de dados pelo numero de id do usuario
+	 * 
+	 * @param id
+	 *            e um inteiro que presenta o numero do id do usuario a ser
+	 *            buscado
+	 * @return um objeto usuario quando encontra e Null quando nao encontra
+	 *         registro
+	 */
+
+	public Usuario buscarPorId(Integer id) {
+
+		String sql = "select * from usuario where id = ?";
+		try (PreparedStatement preparador = con.prepareStatement(sql)) {
+			preparador.setInt(1, id);
+			ResultSet resultado = preparador.executeQuery();
+			// Posicionando cursor no primeiro registro
+			if (resultado.next()) {
+				Usuario usuario = new Usuario();
+				usuario.setId(resultado.getInt("id"));
+				usuario.setNome(resultado.getString("nome"));
+				usuario.setLogin(resultado.getString("login"));
+				usuario.setSenha(resultado.getString("senha"));
+
+				return usuario;
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		return null;
+
+	}
+
+	/**
+	 * Realiza a busca de varios registros da tabela usuario
+	 * 
+	 * @return Uma lista de objetos Usuarios contendo 0 elementos quanto tiver
+	 *         registro ou n elementos quando encontra.
+	 */
+
+	public List<Usuario> buscarTodos() {
+
+		String sql = "select * from usuario";
+		List<Usuario> lista = new ArrayList<Usuario>();
+		try (PreparedStatement preparador = con.prepareStatement(sql)) {
+			ResultSet resultado = preparador.executeQuery();
+			// Posicionando cursor no primeiro registro
+			while (resultado.next()) {
+				Usuario usuario = new Usuario();
+				usuario.setId(resultado.getInt("id"));
+				usuario.setNome(resultado.getString("nome"));
+				usuario.setLogin(resultado.getString("login"));
+				usuario.setSenha(resultado.getString("senha"));
+				// Adicionando usuario na lista
+				lista.add(usuario);
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		return lista;
+
+	}
+
+	public Usuario autenticar(Usuario usuConsulta){
+		
+		String sql = "Select * from usuario where login = ? and senha= ?";
+		
+		try(PreparedStatement preparador = con.prepareStatement(sql)){
+			preparador.setString(1, usuConsulta.getLogin());
+			preparador.setString(2, usuConsulta.getSenha());
+			ResultSet resultado = preparador.executeQuery();
+			
+			if(resultado.next()){
+			Usuario usuario = new Usuario();
+			usuario.setId(resultado.getInt("id"));
+			usuario.setNome(resultado.getString("nome"));
+			usuario.setLogin(resultado.getString("login"));
+			usuario.setSenha(resultado.getString("senha"));
+			
+			return usuario;
+			}
 			
 		} catch (SQLException e) {
 			
 			e.printStackTrace();
 		}
 		
-	}
-
-	public void excluir(Usuario usu){
-	String sql = "delete from usuario where id = ?";
-	
-	try {
-		//Criando um Stattement
-		PreparedStatement preparador = con.prepareStatement(sql);
-		preparador.setInt(1, usu.getId());
-		//Execultando o comando SQL no banco
-		preparador.execute();
-		//Encerando o objeto preparador
-		preparador.close();
+		return null;
 		
-	} catch (SQLException e) {
-		
-		e.printStackTrace();
 	}
-	
-}
 
 }
--- src/test/java/br/com/marciowillian/TestUsuarioDAO.java
+++ src/test/java/br/com/marciowillian/TestUsuarioDAO.java
@@ -1,53 +1,95 @@
 package br.com.marciowillian;
 
+import java.util.List;
+
 import br.com.marciowillian.persistencia.entidade.Usuario;
 import br.com.marciowillian.persistencia.jdbc.UsuarioDAO;
 
 public class TestUsuarioDAO {
 
 	public static void main(String[] args) {
-		testeCadastrar();
+		//testeAlterar();
+		//testwCadastrar();
+		// testeSalvar ();
+		//testeBuscarPorId();
+		testeAltenticar();
+		//testeBuscarTodos();
+	}
+
+	private static void testeAltenticar() {
+		UsuarioDAO usuarioDAO = new UsuarioDAO();
+		
+		Usuario usu = new Usuario();
+		usu.setLogin("marciowillian");
+		usu.setSenha("willianmarcio");
+		
+		Usuario usuRetorno = usuarioDAO.autenticar(usu);
+		
+		System.out.println(usuRetorno);
+		
+	}
+
+	private static void testeBuscarTodos() {
+		UsuarioDAO usuarioDAO = new UsuarioDAO();
+		List<Usuario> lista = usuarioDAO.buscarTodos();
+		
+		for (Usuario u: lista){
+		System.out.println(u);
+		}
 	}
 	
-	public static void testeExcluir(){
-		//Excluindo o Usuario
-				Usuario usu = new Usuario();
-				usu.setId(8);
-				
-				//Excluindo o Usuario no Banco
-				UsuarioDAO usuDAO = new UsuarioDAO();
-				usuDAO.excluir(usu);
-				
-				System.out.println("Usuario excluido com sucesso!!!");
+	private static void testeBuscarPorId() {
+		UsuarioDAO usuarioDAO = new UsuarioDAO();
+		Usuario usuario = usuarioDAO.buscarPorId(5);
+
+		System.out.println(usuario);
+
 	}
+
 	
-	public static void testeAlterar(){
-		//Criando o Usuario
-				Usuario usu = new Usuario();
-				usu.setId(1);
-				usu.setNome("marcio");
-				usu.setLogin("marciowillian");
-				usu.setSenha("willianmarcio");
-				
-				//Cadastrando Usuario no Banco
-				UsuarioDAO usuDAO = new UsuarioDAO();
-				usuDAO.alterar(usu);
-				
-				System.out.println("Usuario alterado com sucesso!!!");
+
+	public static void testeAlterar() {
+		// Criando o Usuario
+		Usuario usu = new Usuario();
+		usu.setId(1);
+		usu.setNome("marcio");
+		usu.setLogin("marciowillian");
+		usu.setSenha("willianmarcio");
+
+		// Cadastrando Usuario no Banco
+		UsuarioDAO usuDAO = new UsuarioDAO();
+		usuDAO.alterar(usu);
+
+		System.out.println("Usuario alterado com sucesso!!!");
 	}
-	
-	public static void testeCadastrar(){
-		//Criando o Usuario
-				Usuario usu = new Usuario();
-				usu.setNome("mr");
-				usu.setLogin("mrrobot");
-				usu.setSenha("robotmr");
-				
-				//Cadastrando Usuario no Banco
-				UsuarioDAO usuDAO = new UsuarioDAO();
-				usuDAO.cadastrar(usu);
-				
-				System.out.println("Cadastrado com sucesso!!!");
+
+	public static void testeCadastrar() {
+		// Criando o Usuario
+		Usuario usu = new Usuario();
+		usu.setNome("mr");
+		usu.setLogin("mrrobot");
+		usu.setSenha("robotmr");
+
+		// Cadastrando Usuario no Banco
+		UsuarioDAO usuDAO = new UsuarioDAO();
+		usuDAO.cadastrar(usu);
+
+		System.out.println("Cadastrado com sucesso!!!");
 	}
-	
+
+	public static void testeSalvar() {
+
+		Usuario usuario = new Usuario();
+		//usuario.setId();
+		usuario.setNome("lucas");
+		usuario.setLogin("lucasserejo");
+		usuario.setSenha("serejolucas");
+
+		UsuarioDAO usuDAO = new UsuarioDAO();
+		usuDAO.salvar(usuario);
+
+		System.out.println("Usuario salvo com sucesso!!!");
+
+	}
+
 }
